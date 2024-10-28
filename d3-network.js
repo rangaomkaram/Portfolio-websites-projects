afb1098b0c4c31d7f80c27f491ae3f83fffd6f00
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-10-28 14:08:18 +0530

    adding and modifying styles and new d3 feature added

---
+++ d3-network.js
@@ -0,0 +1,13 @@
+import {
+    Runtime,
+    Inspector,
+} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js"
+import define from "https://api.observablehq.com/@d3/disjoint-force-directed-graph/2.js?v=4"
+new Runtime().module(define, (name) => {
+    if (name === "chart")
+        return new Inspector(
+            document.querySelector(
+                "#observablehq-chart-5e6c7ca8"
+            )
+        )
+})
